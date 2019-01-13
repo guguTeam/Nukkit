5132a86d87497f34b9f829794600848e979f4470
soeur <1034236490@qq.com>
2019-01-13 09:22:18 +0800

    api update

--- src/main/java/cn/nukkit/apiserver/Api.java
+++ src/main/java/cn/nukkit/apiserver/Api.java
@@ -13,7 +13,8 @@ this.server = server;
 //server实例化完成之后，start之前
 public void init() {
 String ip = server.getConfig("api.ip", "127.0.0.1");
-server.getLogger().info(ip);
+String port = server.getConfig("api.port", "19132");
+server.getLogger().info(port);
 }
 
 //server reload之前
